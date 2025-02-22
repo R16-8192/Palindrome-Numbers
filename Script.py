3089f9d690e0171eb41b1936a10dc5556b127b2e
R16-8192 <r16.8192bit@gmail.com>
2025-02-22 12:37:54 +0100

    Script.py

--- Script.py
+++ Script.py
@@ -1,17 +1,13 @@
-def isPalindrome(x):
-    if x < 0:
-      return False  
-    a = x
+def func(x):
+    z = x
     reverse = 0
-  
-    while x > 0:
-      digit = x % 10
-      reverse = reverse * 10 + digit
-      x //= 10
-        
-    return a == reverse
-inp = int(input("Enter Number : ")
-if isPalindrome(inp):
-    print("Is palindrome")
+    while z > 0:
+        digit = z % 10
+        reverse = reverse * 10 + digit
+        z = z // 10
+    return x == reverse
+y = int(input("Enter Number : "))
+if func(y):
+    print(f"{y} Is Palindrome")
 else:
-    print("Is not palindrome")
+    print(f"{y} is not Palindrome")
